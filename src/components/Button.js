ff7a8999b8bff5c78f7400ae7af47ca49711e23a
Cleyton-1995 <cleytoncosta.developer@gmail.com>
2024-07-01 16:35:29 -0300

    feat: Update the button component

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,13 +2,13 @@ import { FontAwesome } from "@expo/vector-icons";
 import React from "react";
 import { View, StyleSheet, Pressable, Text } from "react-native";
 
-export default function Button({ label, theme }) {
+export default function Button({ label, theme, onPress }) {
    if (theme === "primary") {
      return (
        <View style={[styles.buttonContainer, { borderWidth: 4, borderColor: "#ffd33d", borderRadius: 18 }]}>
          <Pressable
            style={[styles.button, { backgroundColor: "#fff" }]}
-           onPress={() => alert('You pressed a button.')}
+           onPress={onPress}
          >
            <FontAwesome
              name="picture-o"
